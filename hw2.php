cb5af8d75fe11c2fa5d36e4a5cdc5816af98dc61
lkree <xtendmix@gmail.com>
2018-10-11 19:31:20 +0300

    update second hw

--- hw2.php
+++ hw2.php
@@ -4,9 +4,19 @@
         'Antarctica' => ['Aptenodytes forsteri'],
         'Australia' => ['Macropus'],
         'Eurasia' => ['Pterimys volans', 'Bison bonasus', 'Apodemus agrarius'],
-        'North America' => ['Mammuthus columbi', 'Bison bison'],
+        'North America' => ['Mammuthus columbi', 'Stereoson bison'],
         'South America' => ['Eunectes mirunus']
     ];
+
+    $animalsFirstNamesInCountries = [
+        'Africa' => ['Giraffa', 'Loxodonta'],
+        'Antarctica' => ['Aptenodytes'],
+        'Australia' => ['Macropus'],
+        'Eurasia' => ['Pterimys', 'Bison', 'Apodemus'],
+        'North America' => ['Mammuthus', 'Stereoson'],
+        'South America' => ['Eunectes']
+    ];
+    
 
     $twoWordsAnimals = [];
     foreach ($animalsInCountries as $k => $v) {
@@ -21,23 +31,46 @@
     $animalsSurname = [];
     $finalNamesAnimals = [];
 
-    foreach ($twoWordsAnimals as $key => $value) {
-        $symbolsBeforeSecondName = strpos($value, ' ');
-        $animalsSurname[] = mb_substr($value, $symbolsBeforeSecondName);
+    foreach ($twoWordsAnimals as $k => $v) {
+        $animalsFirstname[] = array_pop(explode(' ', $v, -1));
+        $animalsSurname[] = array_pop(explode(' ', $v, 2));
+    }
 
-        $symbolsAfterSecondName = mb_strlen($value) - $symbolsBeforeSecondName - 1;
-        $animalsFirstname[] = mb_substr($value, 0, -($symbolsAfterSecondName));
-    };
- 
-    $tempNumber = count($animalsFirstname);
+    shuffle($animalsFirstname);
+    shuffle($animalsSurname);
 
-    for ($i = 0; $i < $tempNumber; $i++) {
-        shuffle($animalsFirstname);
-        shuffle($animalsSurname);
+    $numberFirstnames = count($animalsFirstname);
 
+    for ($i = 0; $i < $numberFirstnames; $i++) {
         $tempFirstName = array_pop($animalsFirstname);
         $tempSecondName = array_pop($animalsSurname);
 
-        $finalNamesAnimals[] = $tempFirstName . $tempSecondName;
+        $finalNamesAnimals[] = $tempFirstName .' '. $tempSecondName;
     }
-    var_dump($finalNamesAnimals);
+
+    foreach($finalNamesAnimals as $k => $v) {
+        $tempName[] = array_pop(explode(' ', $v, -1));
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+for ($i = 0; $i < count($tempName); $i++) {
+    foreach($animalsFirstNamesInCountries as $k => $v) {
+        foreach($v as $key => $value) {
+            if ($tempName[$i] == $value) { ?>
+    <h2><?=$k?></h2>
+    <p><?=$finalNamesAnimals[$i]?></p>  
+<?php } } } } ?>
+</body>
+</html>
+
