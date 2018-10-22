48a768474d2bbdd618a6a57d986b30d62e90675a
lkree <xtendmix@gmail.com>
2018-10-22 16:42:52 +0300

    fixed errors 3th hw

--- hw3/books.php
+++ hw3/books.php
@@ -1,35 +1,49 @@
 <?php
     function booksApi ($argv) {
         error_reporting(-1); // почему-то ругается на часть массива полученного из гугла (якобы 2 пустых)
+        $booksCsvFile = __DIR__.'/booksAndAuthors.csv';
+
         if (isset($argv[1])) { // проверяем на наличие параметров
             array_shift($argv);
             $stringBookName = '';
+
             foreach ($argv as $k => $v) { // записываем все параметры в строку, на случай составного имени книги
                 $stringBookName .= $v. ' ';
             }
+
             $stringBookName = urlencode(substr($stringBookName, 0, -1)); // переводим строку в часть урл
             $jsonBook = 'https://www.googleapis.com/books/v1/volumes?q=' . $stringBookName;
+
             if (@fopen($jsonBook, "r")) { // проверяем возможность соединиться с удаленным сервером
                 $jsonBook = json_decode(file_get_contents($jsonBook), true); // превращаем полученный json в массив
+
                 if (!(json_last_error())) { // если не было ошибок продолжаем работу
+                    fopen($booksCsvFile, 'w'); // создаем файл для записи и/или открываем его
+
                     foreach ($jsonBook as $k => $v) { // разбираем массив на части
+
                         if ($jsonBook['totalItems'] == 0) { // если книга не найдена
-                            echo 'К сожалению такая книга не найдена :(';
-                            die();
-                        } else {
-                            fopen('booksAndAuthors.csv', 'w'); // создаем файл для записи и/или открываем его
-                            foreach ($v as $key => $value) {
-                                if (!empty($value['volumeInfo']['authors'])) {
-                                    $bookNameAndAuthor = [array_shift($value['volumeInfo']['authors']), $value['volumeInfo']['title']]; // формируем название книги
-                                    $openedCsv = fopen('booksAndAuthors.csv', 'a');
-                                    fputcsv($openedCsv, $bookNameAndAuthor); //записываем в файл поочередно все названия
-                                    fclose($openedCsv);
-                                }
-                            }
+                            die('К сожалению такая книга не найдена :(');
+                        }   
+                    }
+
+                    $openedCsv = fopen($booksCsvFile, 'a');
+
+                    foreach ($jsonBook['items'] as $k => $v) {
+                                
+                        if (!empty($v['volumeInfo']['authors'])) {
+                            $bookNameAndAuthor = [array_shift($v['volumeInfo']['authors']), $v['volumeInfo']['title']]; // формируем название книги
+                            fputcsv($openedCsv, $bookNameAndAuthor); //записываем в файл поочередно всназвания
                         }
-                    } echo 'Книги успешно записаны!';
+                    }
+                    fclose($openedCsv);
+
+                    echo 'Книги успешно записаны!';
+
                 } else echo 'Были обнаружены ошибки при работе с удаленным json файлом';
+
             } else echo 'Нет соединения с сервером.';
+
         } else echo 'Пожалуйста введите название книги.';
     }
     booksApi($argv);
--- hw3/money.php
+++ hw3/money.php
@@ -3,35 +3,55 @@
         error_reporting(-1);
         $currentDate = date('Y-m-d'); // задаем дату
         $fullDayPurchases = 0;
+        $purchasesFile = __DIR__.'/purchases.csv';
+
         if (!empty($argv[1])) {
             $floatPrice = floatval($argv[1]); //изменяем тип цены на флоат (дабл)
+
+            if (!file_exists($purchasesFile)) {
+                $createdCsv = fopen($purchasesFile, 'x');
+                fclose($createdCsv);
+            }
+            
             if ($argv[1] == '--today') { //проверяем первый аргумент на запрос вывода всей дневной суммы
-                if (file(__DIR__.'/purchases.csv')) {
-                    $spends = file(__DIR__.'/purchases.csv', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+                if (file_exists($purchasesFile)) {
+                    $spends = file($purchasesFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
                     foreach ($spends as $k => $v) { // перебираем файл
                         $oneTimePurchase = explode(',', $v);
+
                         if ($oneTimePurchase[0] == $currentDate) { // проверяем дату, при совпадении записываем
                             $fullDayPurchases += $oneTimePurchase[1];
                         } 
-                    } 
+                    }
+
                    if (!empty($fullDayPurchases)) { // если всё ок - выводим
                     echo($currentDate. ' расход за день: '. $fullDayPurchases);
                    } else echo ('Сегодня не было ни одной покупки!'); // если сегодня еще не было покупок - сообщаем
                 }
+
             } elseif ($floatPrice <= 0) { // если что-то с заданными параметрами пошло не так - оповещаем
                 echo ('Ошибка! Аргументы не заданы или заданы неправильно. Укажите флаг --today или запустите скрипт с  аргументами {цена} и {описание покупки}');
+
             } elseif (isset($argv[1]) && isset($argv[2])) { // проверяем наличие цены и купленной вещи
                 array_shift($argv); // обрезаем цену
+
                 $purchaseItem = implode(array_slice($argv, 1), ' '); // записываем имя покупки
                 $purchasePrice = implode(array_slice($argv, 0, 1)); // цену
                 $fullPurchase = [$currentDate, $purchasePrice, $purchaseItem]; // всё вместе
-                if (is_writable('purchases.csv')) { // проверяем на возможность записи
-                  $openedCsv = fopen('purchases.csv', 'a');
-                  fputcsv($openedCsv, $fullPurchase);
-                  fclose($openedCsv);
-                  echo ('Добавлена строка: ' . $fullPurchase[0] .', '. $fullPurchase[1] .', '. $fullPurchase[2]); //  записываем и оповещаем
-                } else echo('Невозможно записать данные! Файл отсутствует или недоступен'); // даем знать, что с файлом     что-то не то
+
+                if (is_writable($purchasesFile)) { // проверяем на возможность записи
+                    $openedCsv = fopen($purchasesFile, 'a');
+
+                    fputcsv($openedCsv, $fullPurchase);
+                    fclose($openedCsv);
+                  
+                    echo ('Добавлена строка: ' . $fullPurchase[0] .', '. $fullPurchase[1] .', '. $fullPurchase[2]); //  записываем и оповещаем
+
+                } else echo('Невозможно записать данные! Файл отсутствует или недоступен'); // даем знать, что с файлом что-то не то
+
             } else echo('Пожалуйста, введите что именно вы купили'); // на случай если нет имени покупки
+
         } else echo ('Ошибка! Аргументы не заданы или заданы неправильно. Укажите флаг --today или запустите скрипт с аргументами {цена} и {описание покупки}');
     };
     money($argv);
