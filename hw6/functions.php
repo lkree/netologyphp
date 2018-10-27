77f50a824776b9e66f3b9a2f4e24c7705c12ffb3
lkree <xtendmix@gmail.com>
2018-10-27 08:08:41 +0300

    form hw6 fix

--- hw6/functions.php
+++ hw6/functions.php
@@ -1,7 +1,10 @@
 <?php
     function formName () { ?>
 <form action="" method="POST">
-    <input type="text" name="usersName">
+    <label for="usersName">
+        Введите ваше имя:
+        <input type="text" name="usersName" id="usersName">
+    </label>
     <input type="submit" name="submit">
 </form>
     <?php }?>
